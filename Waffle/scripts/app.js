e29ae91aa64db9d5f3609b7ed2a9987d7eafefcf
Terry Healy <terence.d.healy@gmail.com>
2014-11-05 00:57:09 +0000

    With first radio buttons.

--- Waffle/scripts/app.js
+++ Waffle/scripts/app.js
@@ -7,24 +7,3 @@ document.addEventListener('deviceready', function () {
   // Cordova will wait 5 very long seconds to do it for you.
   navigator.splashscreen.hide();
 }, false);
-
-
-        $(document).ready(function() {
-            var slider = $("#slider").kendoSlider({
-                increaseButtonTitle: "Right",
-                decreaseButtonTitle: "Left",
-                min: -10,
-                max: 10,
-                smallStep: 2,
-                largeStep: 1
-            }).data("kendoSlider");
-
-            $(".eqSlider").kendoSlider({
-                orientation: "vertical",
-                min: -20,
-                max: 20,
-                smallStep: 1,
-                largeStep: 20,
-                showButtons: false
-            });
-        });
